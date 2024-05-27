b4cd87e56e0f5c4296803bd349dc1ee3cca0af0f
Matthew-Mensah <mmensah427@st.ug.edu.gh>
2024-05-27 12:56:40 +0000

    final commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>My name is <b>Matthew Paa Kwesi Mensah</b></Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'grey',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize:"24px"
+  }
+
+});
---
+++ README.md
@@ -0,0 +1,15 @@
+![alt text](<WhatsApp Image 2024-05-27 at 12.34.57.jpeg>)
+# Matthew Paa Kwesi Mensah
+## 11148680
+
+## Task 1
+An app was created and named it MyApp
+
+## Task 2
+In the text, I changed my name to bold
+
+## Task 3
+I also changed the app background to grey colour
+
+## Task 4
+I then change the fontsize of my name to 24
